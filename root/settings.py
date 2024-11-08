4f91c3f097dea619278fb44727d9701175380fec
SAI909099 <sulaymonovabdulaziz1@gmail.com>
2024-11-08 16:42:49 +0500

    send email work

--- root/settings.py
+++ root/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": "watch_shop",
+        "USER": "postgres",
+        "PASSWORD": "1",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
